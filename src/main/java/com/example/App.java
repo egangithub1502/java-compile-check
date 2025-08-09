6014364f268681ccb6b20b97bb2ab81c02412f94
jegan <2017107071b@gmail.com>
2025-08-09 14:35:31 +0530

    test repo

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Java compilation check passed!");
+        System.out.println("Java Maven build check passed!");
     }
 }
